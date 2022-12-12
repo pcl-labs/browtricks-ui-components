aa348b019027649b858e66624113a41d3b66f1da
Joe Elia <44442621+joeelia@users.noreply.github.com>
2022-12-12 10:30:44 -0800

    cloudflare pages is base sensative...

--- src/views/CustomersView.vue
+++ src/views/CustomersView.vue
@@ -4,7 +4,7 @@
       <TopNavbar :is-login="isLogin" />
       <!-- column wrapper -->
       <div class="flex flex-1 relative items-stretch">
-        <SidebarNavigation />
+        <SideBarNavigation />
         <h1>Customer page</h1>
       </div>
     </div>
@@ -12,12 +12,12 @@
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
+import { ref } from "vue";
 
 import HomeCustomer from '@/components/HomeCustomer.vue';
 import TopNavbar from "@/components/helpers/TopNavbar.vue";
-import SidebarNavigation from "@/components/helpers/SidebarNavigation.vue";
+import SideBarNavigation from "@/components/helpers/SideBarNavigation.vue";
 
-const isLogin = reactive(true);
+const isLogin = ref(true);
 
 </script>
