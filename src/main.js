8b381b5ee8482648ab96608c6cfa4e2cb647b0c8
Joe Elia <44442621+joeelia@users.noreply.github.com>
2022-12-12 10:24:16 -0800

    rebase main into revisions PR

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 import App from './App.vue'
 import router from './router' 
 import 'typeface-inter'
 import './assets/global.scss'
 
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
 app.use(createPinia())
 app.use(router)
