683dabf3095bf62a4928c053f8da6d7a076347ec
Mohammmad Kamran Shekh <kshekh@gmail.com>
2023-02-06 19:22:25 +0530

    reverted back the PmuFormCreate.vue from master

--- src/components/PmuFormCreate.vue
+++ src/components/PmuFormCreate.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="relative flex flex-col w-full flex-1 xs:pr-16">
     <div class="flex-1 space-y-6 xs:pt-0">
-      <div
-        class="overflow-hidden rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8"
-      >
+      <div class="overflow-hidden rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8">
         <div class="space-y-4">
           <div class="relative">
             <Input
@@ -29,17 +27,11 @@
       </div>
       <div class="xs:relative space-y-3 xs:space-y-0">
         <!-- Before Form Upload -->
-        <div
-          class="relative rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8"
-        >
+        <div class="relative rounded-lg shadow-3 bg-white border border-grey-300 p-4 lg:py-6 lg:px-8">
           <div class="space-y-4">
             <div class="space-y-1">
-              <h3 class="flex-1 text-base font-bold leading-snug text-grey-800">
-                Use your own document
-              </h3>
-              <p
-                class="text-sm font-normal text-grey-700 leading-snug md:leading-none"
-              >
+              <h3 class="flex-1 text-base font-bold leading-snug text-grey-800">Use your own document</h3>
+              <p class="text-sm font-normal text-grey-700 leading-snug md:leading-none">
                 Upload your own pre-built PDF or DOC form
               </p>
             </div>
@@ -73,9 +65,7 @@
         </div>
 
         <!-- After Form Upload (Please remove class "hidden" when document will be uploaded and updated here)-->
-        <div
-          class="relative rounded-lg shadow-3 bg-white border border-grey-300 space-y-3 p-4 lg:py-6 lg:px-8 hidden"
-        >
+        <div class="relative rounded-lg shadow-3 bg-white border border-grey-300 space-y-3 p-4 lg:py-6 lg:px-8 hidden">
           <div class="flex items-center">
             <div class="relative flex-1">
               <Input
@@ -97,192 +87,34 @@
           </div>
 
           <div class="relative flex justify-center items-center">
-            <img class="h-auto object-cover" :src="uploadedPdf" alt="" />
+            <img
+              class="h-auto object-cover"
+              :src="uploadedPdf"
+              alt=""
+            />
           </div>
         </div>
       </div>
 
       <!-- Start Question area -->
-      <div>
-        <div
-          v-for="(quest, inq) in moreQuestions"
-          :key="inq"
-          class="relative rounded-lg shadow-3 border border-grey-300 bg-white mb-6"
-        >
-          <div class="setup-guide-area p-4 lg:py-6 lg:px-8">
-            <div
-              class="md:grid md:grid-cols-12 gap-5 xl:gap-10 2xl:gap-20 flex flex-col"
-            >
-              <div class="md:hidden">
-                <SelectItem />
-              </div>
-              <div class="md:col-span-9">
-                <div class="relative space-y-4">
-                  <div class="space-y-3">
-                    <Textarea
-                      name="question"
-                      rows="4"
-                      id="question"
-                      :label-text="`Question ${inq + 1}`"
-                    />
-                    <CheckboxRadio
-                      id="visible"
-                      inputClasses="rounded"
-                      type="checkbox"
-                      checked="checked"
-                      name="visibility"
-                      label-text="This question is required"
-                    />
-                  </div>
-
-                  <!-- If want to repeat questions -->
-                  <div class="hidden">
-                  <div
-                    class="space-y-3 "
-                    v-for="(repq, ind) in repQuestions"
-                    :key="ind"
-                  >
-                    <Textarea
-                      name="question"
-                      rows="4"
-                      id="question"
-                      :label-text="`Question ${ind + 1}`"
-                    />
-                  </div>
-                  <!-- Repeat Question -->
-                  <button type="button"
-                    class="flex items-center cursor-pointer"
-                    @click="repQuestions++"
-                  >
-                    <PlusCircle class="w-4 mr-2 text-grey-700" />
-                    Add more textarea
-                  </button>
-                </div>
-                  <!-- Multiple Choice -->
-                  <div class="space-y-3">
-                    <template
-                      v-for="(repq, ind) in repMultipleOptions"
-                      :key="ind"
-                    > <div class="flex gap-3 items-center">
-                      <CheckboxRadio
-                        :id="`mc-#${ind}`"
-                        inputClasses="rounded-full"
-                        type="radio"
-                        checked="checked"
-                        name="mc"
-                        :label-text="`Multiple Choice #${ind}`"
-                      />
-                      <Button
-                          @click="repMultipleOptions--"
-                          class="text-red hover:bg-red/10 px-2 gap-x-1.5 inline-flex items-center justify-end"
-                          :btn-no-border="true"
-                          :btn-primary="false"
-                        >
-                          <XMarkIcon class="block h-5 w-5" />
-                        </Button>
-                    </div>
-                    </template>
-                    <button type="button"
-                      class="flex items-center cursor-pointer"
-                      @click="repMultipleOptions++"
-                    >
-                      <PlusCircle class="w-4 mr-2 text-grey-700" />
-                      Add more multiple options
-                    </button>
-                  </div>
-                  <!-- Check List -->
-                  <div class="space-y-3">
-                    <template v-for="(repq, ind) in repChecklist" :key="ind">
-                      <div class="flex gap-3 items-center">
-                      <CheckboxRadio
-                        :id="`checklist-#${ind}`"
-                        inputClasses="rounded"
-                        type="checkbox"
-                        checked="checked"
-                        name="checklist"
-                        :label-text="`Checklist #${ind}`"
-                      />
-                      <Button
-                          @click="repChecklist--"
-                          class="text-red hover:bg-red/10 px-2 gap-x-1.5 inline-flex items-center justify-end"
-                          :btn-no-border="true"
-                          :btn-primary="false"
-                        >
-                          <XMarkIcon class="block h-5 w-5" />
-                        </Button>
-                        </div>
-                    </template>
-                
-                    <button type="button"
-                      class="flex items-center cursor-pointer"
-                      @click="repChecklist++"
-                    >
-                      <PlusCircle class="w-4 mr-2 text-grey-700" />
-                      Add more checkbox
-                    </button>
-
-                  
-                  </div>
-
-                  <template v-for="(option, index) in addOptions" :key="index">
-                    <div class="flex items-center">
-                      <div class="relative flex-1">
-                        <Input
-                          id="formName"
-                          type="text"
-                          :label-text="`Option #${index}`"
-                          :placeholder="`Option #${index}`"
-                          :on-change="handleInput"
-                          autocomplete="formName"
-                        />
-                      </div>
-                      <div class="pt-6 flex pl-2">
-                        <Button
-                          @click="addOptions--"
-                          class="text-red hover:bg-red/10 px-2 gap-x-1.5 inline-flex items-center ml-auto justify-end"
-                          :btn-no-border="true"
-                          :btn-primary="false"
-                        >
-                          <XMarkIcon class="block h-5 w-5" />
-                        </Button>
-                      </div>
-                    </div>
-                  </template>
-                  <!-- Repeat options -->
-                  <button
-                    class="flex items-center cursor-pointer"
-                    @click="addOptions++"
-                  >
-                    <PlusCircle class="w-4 mr-2 text-grey-700" />
-                    Add Option
-                  </button>
-                </div>
-              </div>
-              <div class="md:col-span-3">
-                <div class="flex flex-col h-full">
-                  <div class="md:block hidden">
-                    <SelectItem />
-                  </div>
-                  <div class="mt-auto flex justify-start sm:justify-end">
-                    <Button
-                      @click.capture="moreQuestions--"
-                      class="text-red hover:bg-red/10 px-2 gap-x-1.5 inline-flex items-center justify-end"
-                      :btn-no-border="true"
-                      :btn-primary="false"
-                    >
-                      <TrashIcon class="w-4" /> Delete
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Sortable
+        item-key="key"
+        :options="{ handle: '.handle' }"
+        :list="questions"
+      >
+        <template #item="{ index }">
+          <QuestionBuilder @delete-question="removeQuestion(index)" />
+        </template>
+      </Sortable>
       <!-- End Questions area -->
 
       <div class="mt-8">
-        <Button type="submit" :btn-primary="true">Save and create form</Button>
+        <Button
+          type="submit"
+          :btn-primary="true"
+        >
+          Save and create form
+        </Button>
       </div>
     </div>
 
@@ -293,17 +125,13 @@
     >
       <div class="space-y-5">
         <!-- Replace with your content -->
-        <div
-          class="flex items-center justify-center gap-5 border border-dashed border-grey-300 p-5 py-32 rounded-xl"
-        >
+        <div class="flex items-center justify-center gap-5 border border-dashed border-grey-300 p-5 py-32 rounded-xl">
           <div class="flex flex-col gap-2">
             <div class="flex justify-center items-center">
               <file-select v-model="file"></file-select>
             </div>
             <p v-if="file">{{ file.name }}</p>
-            <p class="text-xs text-grey-800/80">
-              Accepts .jpg., png., mp4., mov.
-            </p>
+            <p class="text-xs text-grey-800/80">Accepts .jpg., png., mp4., mov.</p>
           </div>
         </div>
         <!-- /End replace -->
@@ -314,24 +142,20 @@
               src="https://www.w3docs.com/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf"
               frameborder="0"
               class="object-cover aspect-square w-full h-1/4"
-            ></iframe>
+            />
           </div>
           <div class="flex items-start justify-between">
             <div class="space-y-1">
-              <h2 class="text-lg font-normal text-grey-800 leading-none">
-                Pre & Post Brow Care.pdf
-              </h2>
+              <h2 class="text-lg font-normal text-grey-800 leading-none">Pre & Post Brow Care.pdf</h2>
               <p class="text-lg font-normal text-grey-700 flex items-center">
                 PDF
-                <span
-                  class="w-2 h-2 rounded-full bg-grey-500 block mx-3"
-                ></span>
+                <span class="w-2 h-2 rounded-full bg-grey-500 block mx-3" />
                 Jun 4, 2022
               </p>
             </div>
             <a
               href="javascript:void(0)"
-              class="inline-flex items-center justify-center rounded-md border border-grey-300 bg-white text-lg font-medium text-blue shadow-sm hover:bg-blue hover:border-blue hover:text-white focus:outline-none focus:ring-2 focus:ring-grey-300 focus:ring-offset-2 ease-in-out duration-300 w-9 h-9"
+              class="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white text-lg font-medium text-blue shadow-sm hover:bg-blue hover:border-blue hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 ease-in-out duration-300 w-9 h-9"
             >
               <LinkChain class="w-4 h-4" />
             </a>
@@ -347,16 +171,18 @@
             type="submit"
             class="w-auto"
             :btn-primary="true"
-            >Save</Button
           >
+            Save
+          </Button>
           <Button
             class="w-auto"
             @click="isUploadDocument = false"
             type="button"
             :btn-outline="true"
             :btn-primary="false"
-            >Cancel</Button
           >
+            Cancel
+          </Button>
         </div>
 
         <!-- Picture Profile after upload delete media button needs to enable () -->
@@ -364,7 +190,7 @@
           @click="deleteCustomerDailog = true"
           :btn-no-border="true"
           :btn-primary="false"
-          class="text-red hover:bg-red/10 xs:px-4 gap-x-2 inline-flex items-center hidden"
+          class="text-red hover:bg-red/10 xs:px-4 gap-x-2 inline-flex items-center"
         >
           <TrashIcon class="w-4" /> Delete Media
         </Button>
@@ -378,19 +204,14 @@
   >
     <div class="p-1.5 flex xs:flex-col gap-2">
       <Button
-        @click.capture="
-          moreQuestions++;
-          isMoreQuestions = true;
-        "
+        @click="addQuestion"
         class="text-grey-700 hover:bg-red/10 px-2 flex items-center justify-end"
         :btn-no-border="true"
         :btn-primary="false"
       >
         <PlusCircle class="w-5" />
       </Button>
-      <hr
-        class="h-full xs:h-auto xs:w-full border-grey-500 border-0 border-l xs:border-l-0 xs:border-b"
-      />
+      <hr class="h-full xs:h-auto xs:w-full border-grey-500 border-0 border-l xs:border-l-0 xs:border-b" />
       <Button
         class="text-grey-700 hover:bg-red/10 px-2 flex items-center justify-end"
         :btn-no-border="true"
@@ -405,30 +226,32 @@
 
 <script setup>
 import { ref } from 'vue';
-import { reactive } from 'vue';
+import { Sortable } from 'sortablejs-vue3';
 import Input from '@/components/Input.vue';
 import Textarea from '@/components/Textarea.vue';
 import SlideOvers from '@/components/SlideOvers.vue';
 import FileSelect from '@/components/FileSelect.vue';
 import Button from '@/components/Button.vue';
-import CheckboxRadio from '@/components/CheckboxRadio.vue';
-import SelectItem from '@/components/SelectItem.vue';
-import XMarkIcon from '@/assets/icons/XMarkIcon.vue';
 import TrashIcon from '@/assets/icons/trash.vue';
 import PlusCircle from '@/assets/icons/PlusCircle.vue';
 import PictureIcon from '@/assets/icons/PictureIcon.vue';
 import LinkChain from '@/assets/icons/LinkChain.vue';
 import uploadedPdf from '@/assets/Pre_and_Post_Care_Agreement.png';
+import QuestionBuilder from './pmu/QuestionBuilder.vue';
+
 const moreQuestions = ref(0);
-const repQuestions = ref(0);
-const repChecklist = ref(1);
-const repMultipleOptions = ref(1);
-const addOptions = ref(1);
 const isMoreQuestions = ref(false);
 const isUploadDocument = ref(false);
-const state = reactive({ count: 0 });
 
-function increment() {
-  state.count++;
+const questions = ref([{ key: 1 }]);
+
+function addQuestion() {
+  let lastKey = questions.value[questions.value.length - 1]?.key;
+  lastKey = lastKey === null ? 1 : lastKey++;
+  questions.value.push({ key: lastKey });
+}
+
+function removeQuestion(index) {
+  questions.value.splice(index, 1);
 }
 </script>
